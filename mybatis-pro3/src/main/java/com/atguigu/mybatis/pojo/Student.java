95914b66a1d5b99cfce1862413475d5a17fcf961
zhenglf <417812279@qq.com>
2023-06-13 16:30:09 +0800

    first commit

---
+++ mybatis-pro3/src/main/java/com/atguigu/mybatis/pojo/Student.java
@@ -0,0 +1,18 @@
+package com.atguigu.mybatis.pojo;
+
+/**
+ * ClassName:Student
+ * Package: com.atguigu.mybatis.pojo
+ * Description :
+ *
+ * @Author: zlf
+ * @Create 2023/6/13 - 16:29
+ * @Version: v1.0
+ */
+public class Student {
+    private Integer id;
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
